48bb1d924393572606f79da69a035ad60e68bdbd
sunflail <sunflail2001@yahoo.com>
2021-04-07 16:37:43 -0700

    first stab at methods

--- settings.go
+++ settings.go
@@ -1,12 +1,25 @@
 package settings
 
-import "os"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
 
 func Read(opts ReadOptions, s *interface{}) error {
 	// read in base path (should be the base config file)
-
+	if err := readBaseSettings(opts.BasePath); err != nil {
+		log.Fatal(err)
+	}
 	// apply default mapped values
-
+	defOptions, err := readDefaultsMap(opts.BasePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	opts.SetDefaultsMap(defOptions, true)
 	// apply environment override files
 
 	// apply environment variables
@@ -27,5 +40,22 @@ func readBaseSettings(path string) error {
 		return err
 	}
 
+	Options().SetBasePath(path)
+
 	return nil
+}
+
+func readDefaultsMap(path string) (map[string]interface{}, error) {
+	yamlConfig, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(map[string]interface{})
+	err = yaml.Unmarshal(yamlConfig, &c)
+	if err != nil {
+		return nil, fmt.Errorf("in file %q: %v", path, err)
+	}
+
+	return c, nil
 }
